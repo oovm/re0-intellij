a21f10cf35ec4f1464ef04aac5ec052dc731bd8f
GitHub Action <action@github.com>
2022-07-25 01:19:53 +0000

    Template cleanup

--- src/main/kotlin/com/github/oovm/re0intellij/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/oovm/re0intellij/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.oovm.re0intellij.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.oovm.re0intellij.MyBundle
 
 class MyApplicationService {
 
